ef1f8a74301716bbaa3e6bdf7e061555787b7663
yashGoyal40 <yashg4118@gmail.com>
2024-10-01 13:23:27 +0530

    final

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -88,7 +88,7 @@ const SignUp = () => {
         username:formData.email,
         password:formData.password
       }); // Automatically sign in after confirmation
-      dispatch(login())
+      dispatch(login(formData.email))
       navigate("/")
     } catch (error) {
       setMessage(`Error: ${error.message}`);
